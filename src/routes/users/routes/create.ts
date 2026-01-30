46bc7626c8852d2197ee562f37b4f799ad8cd8b3
Sasindu Sanka <sasindusanka@gmail.com>
2026-01-30 15:58:50 +0530

    update create user route

--- src/routes/users/routes/create.ts
+++ src/routes/users/routes/create.ts
@@ -1,18 +1,50 @@
+import {  createRoute, z } from '@hono/zod-openapi';
+import { status } from '@poppanator/http-constants';
+import { error400, error401, error500 } from '@/openapi/common';
+import { createHonoApp } from '@/lib/create-app';
 
-import { Hono } from 'hono';
-import { zValidator } from '@hono/zod-validator';
-import { z } from 'zod';
+const app = createHonoApp();
 
-const app = new Hono();
-
-const schema = z.object({
-	name: z.string(),
-	email: z.string().email(),
+const createUserSchema = z.object({
+	name: z.string().openapi({ example: 'John' }),
+	email: z.email().openapi({ example: 'johnexample.com' }),
 });
 
-app.post('/', zValidator('json', schema), (c) => {
+const route = createRoute({
+	method: 'post',
+	path: '/',
+	tags: ['Users'],
+	summary: 'Create a new user',
+	request: {
+		body: {
+			content: {
+				'application/json': {
+					schema: createUserSchema,
+				},
+			},
+		},
+	},
+	responses: {
+		[status.Created]: {
+			content: {
+				'application/json': {
+					schema: z.object({
+						message: z.string(),
+						user: createUserSchema,
+					}),
+				},
+			},
+			description: 'User successfully created',
+		},
+		[status.BadRequest]: error400,
+		[status.Unauthorized]: error401,
+		[status.InternalServerError]: error500,
+	},
+});
+
+app.openapi(route, async (c) => {
 	const data = c.req.valid('json');
-	return c.json({ message: 'User Created', user: data });
+	return c.json({ message: 'User Created', user: data }, status.Created);
 });
 
 export default app;
