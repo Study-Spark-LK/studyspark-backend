65771dee191a2b686b2f6a3100c55b503dce96e3
Sasindu Sanka <sasindusanka@gmail.com>
2026-02-09 00:20:08 +0530

    add me query

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import { cors } from 'hono/cors';
 // sub apps
 import userApp from '@/routes/users';
 import webhookApp from '@/routes/webhooks';
+import libraryApp from '@/routes/library';
 import devApp from '@/routes/dev-only';
 
 const app = createHonoApp();
@@ -16,6 +17,13 @@ app.use('*', clerkMiddleware());
 // routes
 app.route('/api/users', userApp);
 app.route('/api/webhooks', webhookApp);
+app.route('/api/library', libraryApp);
+
+app.openAPIRegistry.registerComponent('securitySchemes', 'Bearer', {
+	type: 'http',
+	scheme: 'bearer',
+	bearerFormat: 'JWT',
+});
 
 app.doc('/doc', {
 	openapi: '3.0.0',
@@ -23,7 +31,17 @@ app.doc('/doc', {
 		version: '1.0.0',
 		title: 'StudySpark API',
 	},
-});
+	components: {
+		securitySchemes: {
+			Bearer: {
+				type: 'http',
+				scheme: 'bearer',
+				bearerFormat: 'JWT',
+			},
+		},
+	},
+	security: [{ Bearer: [] }],
+} as any);
 
 app.route('/dev', devApp);
 
