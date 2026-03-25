6731776723008e4fca1f30801d42415fb8400fb4
Aloka Wickramanayake <Dahamidew@gmail.com>
2026-03-25 23:06:51 +0530

    add profile analytics route

---
+++ src/routes/profiles/routes/analytics.ts
@@ -0,0 +1,16 @@
+import { getHonoInstance } from '@/hono';
+import {
+    _401Describe,
+    _404Describe,
+    _500Describe,
+} from '@/openapi';
+
+
+
+
+export function setupGetProfileAnalyticsRoute() {
+    const app = getHonoInstance();
+
+
+
+}
