0fd54e2da350cf3f5bde817db51ac58421136480
Diwyani Gunarathna <yesmigunarathna@gmail.com>
2026-03-25 22:14:21 +0530

    add schema

--- src/routes/documents/lesson.ts
+++ src/routes/documents/lesson.ts
@@ -1,5 +1,14 @@
 import { getHonoInstance } from '@/hono';
 import { createRoute } from '@hono/zod-openapi';
+import { z } from 'zod';
+
+const LessonSchema = z.object({
+    id: z.string(),
+    documentId: z.string(),
+    title: z.string().nullable().optional(),
+    content: z.string(),
+    createdAt: z.number().optional()
+});
 
 
 export function setupGetLessonRoute() {
