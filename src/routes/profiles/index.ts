72f8275a0194348130e99bcbbe534e308567b782
Aloka Wickramanayake <Dahamidew@gmail.com>
2026-03-25 23:54:35 +0530

    export function

--- src/routes/profiles/index.ts
+++ src/routes/profiles/index.ts
@@ -2,10 +2,12 @@ import { setupGetProfilesRoute } from '@/routes/profiles/routes/get-profiles';
 import { setupCreateProfileRoute } from '@/routes/profiles/routes/create-profile';
 import { setupDeleteProfileRoute } from '@/routes/profiles/routes/delete-profile';
 import { setupUpdateProfileRoute } from '@/routes/profiles/routes/update-profile';
+import { setupGetProfileAnalyticsRoute } from './routes/analytics';
 
 export function setupProfileRoutes() {
 	setupGetProfilesRoute();
 	setupCreateProfileRoute();
 	setupDeleteProfileRoute();
 	setupUpdateProfileRoute();
+	setupGetProfileAnalyticsRoute();
 }
